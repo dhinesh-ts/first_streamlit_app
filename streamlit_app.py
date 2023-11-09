8a4f4773f700baa9bc9dfbd1a6eea47fc0f6bf99
dhinesh-ts <150331584+dhinesh-ts@users.noreply.github.com>
2023-11-09 14:09:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('CRD Healthy Diner')
 streamlit.header('Breakfast Menu <3')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
-streamlit.header('Build your own SMOOTHIEEE!!')
+streamlit.header('Build your own SMOOTHIEEE!!🥑🍞')
+
